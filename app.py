3931e7171f4e072e0865a96235165849f31bc137
harshalgrover07 <harshalgrover07@gmail.com>
2025-11-15 16:20:02 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-
-st.title("Calculate your BMI")
-wt = st.number_input("Enter your Weight in kg:")
-h = st.number_input("Enter your Height in m:")
-if h == 0:
-    st.error("Height cannot be zero")
-bmi = wt/h**2
-st.success(f"Your BMI is {bmi}KG/m^2")
